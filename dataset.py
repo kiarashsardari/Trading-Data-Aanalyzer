e1e39e6be5b99509a732468362739124b32f7e78
Kiarash <kiarash123sardari@gmail.com>
2026-09-16 20:53:39 +0330

    update

--- dataset.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = (pd.read_excel(r'my_dataset.xlsx'))
-df.to_csv('my_dataset.csv', index=False, encoding='utf-8-sig')
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import analyzer
 import pandas as pd
-df = pd.read_csv(r'D:\Python\Dash Abolfazl s project\Sample dataset\my_dataset.csv',index_col=0)
+df = pd.read_csv(r'my_dataset.csv',index_col=0)
 print(f'Number of each strategy:\n{(analyzer.count_strategies(df)).to_string(header=None)}\n')
 analyzer.analyze_win_rate(df)
