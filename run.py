466386ffdd84b19218d1d260f83cba31cf6a7421
Kiarash <kiarash123sardari@gmail.com>
2026-09-17 19:35:22 +0330

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 import analyzer
 import pandas as pd
 df = pd.read_csv(r'my_dataset.csv',index_col=0)
-print(f'Number of each strategy:\n{(analyzer.count_strategies(df)).to_string(header=None)}\n')
-analyzer.analyze_win_rate(df)
+
+#Total trades per strategy
+print(f'- Total trades per strategy -\n{(analyzer.count_strategies(df)).to_string(header=None)}\n\n')
+
+#Analyze win rate
+print('- Analyze win rate - \n')
+for dic in (analyzer.analyze_win_rate(df)):
+    print(f'Strategy: {dic['Strategy']} | Stop Loss: {dic['SL count']} | Take Profit: {dic['TP count']} | Win Rate: {dic['Win Rate']} |\n')
