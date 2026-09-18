3cbd298507b6baf59af614160a5f79eb1654a5ff
Kiarash <kiarash123sardari@gmail.com>
2026-09-18 21:29:37 +0330

    Update run.py

--- run.py
+++ run.py
@@ -3,9 +3,24 @@ import pandas as pd
 df = pd.read_csv(r'my_dataset.csv',index_col=0)
 
 #Total trades per strategy
-print(f'- Total trades per strategy -\n{(analyzer.count_strategies(df)).to_string(header=None)}\n\n')
+print(f'--- Total trades per strategy ---\n{(analyzer.count_strategies(df)).to_string(header=None)}\n\n')
 
 #Analyze win rate
-print('- Analyze win rate - \n')
+print('--- Analyze win rate --- \n')
 for dic in (analyzer.analyze_win_rate(df)):
-    print(f'Strategy: {dic['Strategy']} | Stop Loss: {dic['SL count']} | Take Profit: {dic['TP count']} | Win Rate: {dic['Win Rate']} |\n')
+    print(f"Strategy: {dic['Strategy']} | Stop Loss: {dic['SL count']} | Take Profit: {dic['TP count']} | Win Rate: {dic['Win Rate']} |\n\n")
+print('')
+
+#Analyze exit rate
+print('--- Analyze exit rate --- \n')
+grp = analyzer.tp_exit_rate(df)
+print('position : count × andis = exit rate\n')
+for pos, rate in grp.items():
+	print(f'{pos} : {rate}\n')
+print('')
+
+#Best tps
+print('--- Analyze best tps --- \n')
+lst = analyzer.best_tp(df)
+for i in lst:
+	print(i)
