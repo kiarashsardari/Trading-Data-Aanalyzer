5e038cedb3b906982fb8e5255f042848b7f8e868
Kiarash <kiarash123sardari@gmail.com>
2026-09-19 17:22:58 +0330

    Update dataset_maker.py

--- dataset_maker.py
+++ dataset_maker.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-'''df = (pd.read_excel(r'D:\Python\Trading-Data-Aanalyzer Project\Sample dataset\my_dataset.csv.xlsx'))'''
 dates = np.arange('2025-07-01', '2026-02-01', dtype='datetime64[D]')
 l =  len(dates)
 positions = np.random.choice(['SL', 'TP1', 'TP2', 'TP3', 'TP4', 'TP5', 'TP6', 'TP7', 'TP8', 'TP9', 'TP10', 'TP11', 'TP12', 'TP13', 'TP14', 'TP15', 'TP16', 'TP17', 'TP18', 'TP19', 'TP20'], size=l, p=[0.3, 0.1, 0.3, 0.1, 0.029227,0.024321,0.021098,0.018765,0.016543,0.014987,0.013210,0.011876,0.010345,0.009123,0.007890,0.006543,0.005432,0.004321,0.003210,0.002109,0.001000])
@@ -10,4 +9,4 @@ data = {
     ,'position': positions
     ,'strategy': strategies
 }
-(pd.DataFrame(data)).to_csv(r'D:\Python\Trading-Data-Aanalyzer Project\Sample dataset\my_dataset.csv', index=False, encoding='utf-8-sig')
+(pd.DataFrame(data)).to_csv(r'my_dataset.csv', index=False, encoding='utf-8-sig')
